e76159c793642b619db385c24239a1204a1270da
YU-TA <y.matsuo.dev@gmail.com>
2020-02-15 14:21:13 +0900

    feature:2/15更新分反映

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -11,12 +11,30 @@ class IncomesController < ApplicationController
   end
 
   def edit
+    @income = Income.find(params[:id])
+  end
+
+  def update
+    income = Income.find(params[:id])
+    income.update!(income_params)
+    redirect_to incomes_url, notice:"更新しました。"
   end
 
   def create
-    # ↓修正する必要あり
-    @income = Income.new(params[:income])
-    @income.save
-    redirect_to incomes_path
+    income = Income.new(income_params)
+    income.save
+    redirect_to incomes_url,notice: "収入「¥#{income.value}」を登録しました。"
+  end
+
+  def destroy
+    income = Income.find(params[:id])
+    income.destroy
+    redirect_to incomes_url, notice: "収入ID「#{income.id}}」を削除しました。"
+  end
+
+  private
+
+  def income_params
+    params.require(:income).permit(:value,:detail)
   end
 end
